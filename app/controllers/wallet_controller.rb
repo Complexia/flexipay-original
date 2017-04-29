c8a8be85cc7b39badf01ba26e20a9d03a4af52f8
s3399752 <s3399752@student.rmit.edu.au>
2017-04-29 14:07:10 +1000

    changed some stuff so that /wallet isn't accessible unless logged in

--- app/controllers/wallet_controller.rb
+++ app/controllers/wallet_controller.rb
@@ -1,6 +1,18 @@
 class WalletController < ApplicationController
+
+  before_action :logged_in_user
+  
   def balance
 
-    
+
+  end
+
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
   end
 end
